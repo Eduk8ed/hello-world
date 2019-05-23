5a65490719e7ab7c4b19c9ecf3c461440a15cd99
Shantell Barge <Shantellbarge@gmail.com>
2019-05-22 18:18:48 -0700

    idk

--- blog/index.js
+++ blog/index.js
@@ -1,18 +1,24 @@
-newFunction();
+const post = {
+    'title': 'idk',
+    'author': 'i dont care anymore',
+    'intro': 'what a why to make a lady feel slow'
+};
 
-function newFunction() {
-    console.log('Hey There!');
-    var MyArray = [1, 2, "3"];
-    var myFaves = {
-    pet: "dog",
-        drink: "warter",
-        number: "33",
-        array: MyArray,
-        list: myFaves
-    };
+function Post(post){
+    return `<div> 
+        <h1>{$post.intro}</h1>
+        <h3>{$post.author}</h3>
+        <p>{$post.title}</p>
+         </div>`;
 }
-cimm = 100
-if (Password<5)
-   {alert("Please Re-Enter")}
-   else {alert("Thank You")};
-   
+let content = '';
+
+for(let i = 0; i < post.length; i++){
+    content = `${content}${post[i]}`;
+}
+    
+console.log(content);
+
+// by placing the i within the [] it will return all elements of post//
+// or use _post_.length will do the same//
+document.querySelector('#content').innerHTML = Post(post);
