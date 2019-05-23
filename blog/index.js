e40fd97930b988ec066e36c87577d2572448d144
Shantell Barge <Shantellbarge@gmail.com>
2019-05-22 19:00:12 -0700

    forloopjs

--- blog/index.js
+++ blog/index.js
@@ -1,24 +1,52 @@
-const post = {
+const posts = [ {
     'title': 'idk',
     'author': 'i dont care anymore',
     'intro': 'what a why to make a lady feel slow'
-};
+},
+{
+    'title': 'blahhhh',
+    'author': 'i dont care anymore',
+    'intro': 'what a why to make a lady feel slow'
+} ,{
+    'title': 'idk',
+    'author': 'i dont care anymore',
+    'intro': 'what a why to make a lady feel slow'
+},
+
+{
+    'title': 'going crazy here',
+    'author': 'i dont care anymore',
+    'intro': 'what a why to make a lady feel slow'
+},
+{
+    'title': 'Theres no crying in coding',
+    'author': 'i dont care anymore',
+    'intro': 'what a why to make a lady feel slow'
+}  ];
+
 
 function Post(post){
-    return `<div> 
-        <h1>{$post.intro}</h1>
-        <h3>{$post.author}</h3>
-        <p>{$post.title}</p>
-         </div>`;
+    return `
+        <div> 
+            <h1>${post.title}</h1>
+            <h3>${post.author}</h3>
+            <p>${post.intro}</p>
+        </div>
+    `;
 }
-let content = '';
 
-for(let i = 0; i < post.length; i++){
-    content = `${content}${post[i]}`;
+function Content(items){
+    let content = '';
+
+    for(let i = 0; i < items.length; i++){
+        content = `${content}${Post(items[i])}`;
+    }
+    
+    return content;
+    // content ends there never to be felt again.. so to say//
 }
     
-console.log(content);
 
 // by placing the i within the [] it will return all elements of post//
 // or use _post_.length will do the same//
-document.querySelector('#content').innerHTML = Post(post);
+document.querySelector('#content').innerHTML = Content(posts);
